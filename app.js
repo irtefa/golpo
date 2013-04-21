4143696a6f81e2b3b4f160af1bf9c7840a75ff8e
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-20 23:04:12 -0500

    added user list

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var socket = require('socket.io');
 var io = socket.listen(8000);
 
+var clients = [];
+var messages = [];
 
 io.sockets.on('connection', function(client) {
     client.on('join', function(name){
         client.set('nickname', name);
+        clients.push(name);
     });
-    client.on('message', function(msg) {
+    io.sockets.emit('users', clients, messages);
+    client.on('message', function(msg, time) {
         client.get('nickname', function(err, name){
             io.sockets.emit('chat', {'name': name, 'msg': msg});
+            messages.push({'name': name, 'msg': msg, 'time': time});
         });
     });
 });
--- client.js
+++ client.js
@@ -2,6 +2,15 @@ var socket = io.connect('http://localhost:8000');
 
 socket.on('connect', function(){
     var name = prompt("What is your name?");
+    socket.on('users', function(clients, messages){
+        for(i=0; i < clients.length; i++){
+            $('#user-list').append("<li>" + clients[i] + "</li><hr>");
+        }
+        for(i=0; i < messages.length; i++){
+            $('#chat-list').append("<p>" + messages[i].name +": " + messages[i].msg +"</p> <p>Posted at: " + messages[i].time+ "</p><hr>");
+        }
+    });
+    $('#user-list').append("<li>" + name + "</li><hr>");
     socket.emit('join', name);
 });
 
@@ -12,7 +21,7 @@ $('#submit-bngl').click(function(e) {
         alert("Cannot send empty messages!");
     }
     else{
-        socket.emit('message', msg);
+        socket.emit('message', msg, moment().format('MMMM Do YYYY, h:mm:ss a'));
     }
 });
 
@@ -23,7 +32,7 @@ $('#submit-engl').click(function(e) {
         alert("Cannot send empty messages!");
     }
     else{
-        socket.emit('message', msg);
+        socket.emit('message', msg, moment().format('MMMM Do YYYY, h:mm:ss a'));
     }
 });
 
