9a4f9bd4f2c4024e4cc96046ba99bdca69d966e4
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-24 21:02:40 -0500

    added node modules for client side access to socket

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var socket = require('socket.io');
-var io = socket.listen(8000);
-
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+//var app = express().listen(8000);
+server.listen(8000);
 var clients = [];
 var messages = [];
+
+app.configure(function() {
+  app.use(express.static(__dirname));
+});
+
+app.get('/', function(request, respond) {
+    response.sendfile(__dirname + 'index.html');
+});
+var io = socket.listen(server);
 
 io.sockets.on('connection', function(client) {
 
